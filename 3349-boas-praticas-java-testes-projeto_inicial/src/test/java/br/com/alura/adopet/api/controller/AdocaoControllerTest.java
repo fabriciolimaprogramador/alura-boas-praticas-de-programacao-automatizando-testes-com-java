1fa6602a850ccc1c5c28c7622a5e8bb586dcddea
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2023-10-16 08:28:57 -0300

    Teste unitário classe Controller

---
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/test/java/br/com/alura/adopet/api/controller/AdocaoControllerTest.java
@@ -0,0 +1,69 @@
+package br.com.alura.adopet.api.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import br.com.alura.adopet.api.service.AdocaoService;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AdocaoControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@MockBean
+	private AdocaoService service;
+
+	@Test
+	void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoComErros() throws Exception {
+		
+		//ARRENGE
+		String json = "{}";
+		
+		//ACT
+		MockHttpServletResponse response = mvc.perform(
+				post("/adocoes")
+				.content(json)
+				.contentType(MediaType.APPLICATION_JSON)
+				).andReturn().getResponse();
+		
+		//ASSERT
+		Assertions.assertEquals(400, response.getStatus());
+
+	}
+	
+	@Test
+	void deveriaDevolverCodigo400ParaSolicitacaoDeAdocaoSemErros() throws Exception {
+		
+		//ARRENGE
+		String json = """
+				{
+					"idPet": 1,
+					"idTutor": 1,
+					"motivo": "Motivo qualquer"
+				}
+				""";
+		
+		//ACT
+		MockHttpServletResponse response = mvc.perform(
+				post("/adocoes")
+				.content(json)
+				.contentType(MediaType.APPLICATION_JSON)
+				).andReturn().getResponse();
+		
+		//ASSERT
+		Assertions.assertEquals(200, response.getStatus());
+		
+	}
+
+}
