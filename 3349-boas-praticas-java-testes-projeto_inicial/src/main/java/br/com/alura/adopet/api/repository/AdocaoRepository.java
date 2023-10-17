6f6c9f231b8d8e975b41abdeabdbcfb49ce40bad
Fabrício Guimarães de Lima <premiersoft-fabricio.lima@ailos.coop.br>
2023-10-17 09:03:19 -0300

    Teste unitários das classes de Validacao

--- 3349-boas-praticas-java-testes-projeto_inicial/src/main/java/br/com/alura/adopet/api/repository/AdocaoRepository.java
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/main/java/br/com/alura/adopet/api/repository/AdocaoRepository.java
@@ -1,11 +1,16 @@
 package br.com.alura.adopet.api.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import br.com.alura.adopet.api.model.Adocao;
 import br.com.alura.adopet.api.model.StatusAdocao;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdocaoRepository extends JpaRepository<Adocao, Long> {
 
     boolean existsByPetIdAndStatus(Long idPet, StatusAdocao status);
+    
+    boolean existsByTutorIdAndStatus(Long idTutor, StatusAdocao status);
+    
+    Long countByTutorIdByStatus(Long idTutor, StatusAdocao status);
 
 }
--- 3349-boas-praticas-java-testes-projeto_inicial/src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComAdocaoEmAndamento.java
+++ 3349-boas-praticas-java-testes-projeto_inicial/src/main/java/br/com/alura/adopet/api/validacoes/ValidacaoTutorComAdocaoEmAndamento.java
@@ -1,16 +1,12 @@
 package br.com.alura.adopet.api.validacoes;
 
-import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
-import br.com.alura.adopet.api.exception.ValidacaoException;
-import br.com.alura.adopet.api.model.Adocao;
-import br.com.alura.adopet.api.model.StatusAdocao;
-import br.com.alura.adopet.api.model.Tutor;
-import br.com.alura.adopet.api.repository.AdocaoRepository;
-import br.com.alura.adopet.api.repository.TutorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
+import br.com.alura.adopet.api.dto.SolicitacaoAdocaoDto;
+import br.com.alura.adopet.api.exception.ValidacaoException;
+import br.com.alura.adopet.api.model.StatusAdocao;
+import br.com.alura.adopet.api.repository.AdocaoRepository;
 
 @Component
 public class ValidacaoTutorComAdocaoEmAndamento implements ValidacaoSolicitacaoAdocao {
@@ -18,17 +14,25 @@ public class ValidacaoTutorComAdocaoEmAndamento implements ValidacaoSolicitacaoA
     @Autowired
     private AdocaoRepository adocaoRepository;
 
-    @Autowired
-    private TutorRepository tutorRepository;
+//    @Autowired
+//    private TutorRepository tutorRepository;
 
     public void validar(SolicitacaoAdocaoDto dto) {
-        List<Adocao> adocoes = adocaoRepository.findAll();
-        Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
-        for (Adocao a : adocoes) {
-            if (a.getTutor() == tutor && a.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
-                throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
-            }
+        
+        if(adocaoRepository.existsByTutorIdAndStatus(dto.idTutor(), StatusAdocao.AGUARDANDO_AVALIACAO)) {
+        	throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
         }
+        
     }
+
+//    public void validar(SolicitacaoAdocaoDto dto) {
+//    	List<Adocao> adocoes = adocaoRepository.findAll();
+//    	Tutor tutor = tutorRepository.getReferenceById(dto.idTutor());
+//    	for (Adocao adocao : adocoes) {
+//    		if (adocao.getTutor() == tutor && adocao.getStatus() == StatusAdocao.AGUARDANDO_AVALIACAO) {
+//    			throw new ValidacaoException("Tutor já possui outra adoção aguardando avaliação!");
+//    		}
+//    	}
+//    }
 
 }
